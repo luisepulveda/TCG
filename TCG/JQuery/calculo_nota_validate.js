8ddeba70e4d08edea3035bd6125ca7f022d92eec
luis <luif.sepulveda@duocuc.cl>
2024-05-13 21:46:04 -0400

    calculo de nota Jquery

---
+++ TCG/JQuery/calculo_nota_validate.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    $(document).ready(function() {
+        $('#add-note-btn').click(function() {
+          var newNoteDiv = '<div class="mb-3"><label for="nota' + 
+          (parseInt($('.additional-notes div').length) + 1) + 
+          '" class="form-label">Nota ' + 
+          (parseInt($('.additional-notes div').length) + 1) + 
+          ':</label><input type="number" class="form-control" id="nota' + 
+          (parseInt($('.additional-notes div').length) + 1) + 
+          '" min="0" max="100" required></div>';
+          
+          $('.additional-notes').append(newNoteDiv);
+        });
+    });
+
+});
