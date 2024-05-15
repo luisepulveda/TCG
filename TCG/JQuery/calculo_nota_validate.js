d0b046cc1d3e5a9e8f29aa55d66f183f048511c7
luis <luif.sepulveda@duocuc.cl>
2024-05-14 23:43:52 -0400

    Añadir en calculo

--- TCG/JQuery/calculo_nota_validate.js
+++ TCG/JQuery/calculo_nota_validate.js
@@ -1,16 +1,13 @@
 $(document).ready(function() {
-    $(document).ready(function() {
-        $('#add-note-btn').click(function() {
-          var newNoteDiv = '<div class="mb-3"><label for="nota' + 
-          (parseInt($('.additional-notes div').length) + 1) + 
-          '" class="form-label">Nota ' + 
-          (parseInt($('.additional-notes div').length) + 1) + 
-          ':</label><input type="number" class="form-control" id="nota' + 
-          (parseInt($('.additional-notes div').length) + 1) + 
-          '" min="0" max="100" required></div>';
+    $('#add-note-btn').click(function() {
+        var newNoteDiv = '<div class="mb-3"><label for="nota' + 
+        (parseInt($('.additional-notes div').length) + 1) + 
+        '" class="form-label">Nota ' + 
+        (parseInt($('.additional-notes div').length) + 1) + 
+        ':</label><input type="number" class="form-control" id="nota' + 
+        (parseInt($('.additional-notes div').length) + 1) + 
+        '" min="0" max="100" required></div>';
           
-          $('.additional-notes').append(newNoteDiv);
-        });
+        $('.additional-notes').append(newNoteDiv);
     });
-
 });
