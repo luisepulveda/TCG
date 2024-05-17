ced5409b5afe393ab52992a146f3e0ba0f827aab
luis <luif.sepulveda@duocuc.cl>
2024-05-17 01:09:15 -0400

    mas de calculo de notas

--- TCG/JQuery/calculo_nota_validate.js
+++ TCG/JQuery/calculo_nota_validate.js
@@ -4,13 +4,67 @@ $(document).ready(function() {
         (parseInt($('.additional-notes div').length) + 1) + 
         '" class="form-label">Nota ' + 
         (parseInt($('.additional-notes div').length) + 1) + 
-        ':</label><input type="number" class="form-control" id="nota' + 
+        ':</label><input type="number" class="form-control" id="nota'+ 
         (parseInt($('.additional-notes div').length) + 1) + 
         '" min="0" max="100" required></div>';
           
         $('.additional-notes').append(newNoteDiv);
     });
+
     $('#delete-note').click(function(){
             $('.additional-notes div:last-child').remove();
         });
+    $('#calculator-form').validate({
+        rules:{
+            nota1:{
+                required:true,
+                min:20,
+                max:70
+            },
+            nota2:{
+                required:true,
+                min:20,
+                max:70
+            },
+            nota3:{
+                required:true,
+                min:20,
+                max:70
+            },
+            nota4:{
+                required:true,
+                min:20,
+                max:70
+            },
+            nota5:{
+                required:true,
+                min:20,
+                max:70
+            },
+            nota6:{
+                required:true,
+                min:20,
+                max:70
+            },
+            nota7:{
+                required:true,
+                min:20,
+                max:70
+            },
+            nota8:{
+                required:true,
+                min:20,
+                max:70
+            },
+            nota9:{
+                required:true,
+                min:20,
+                max:70
+            }
+        },
+        messages: {
+            nota1:
+            requared
+        }
+    });
 });
