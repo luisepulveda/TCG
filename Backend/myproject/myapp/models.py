622d0809c587887fcf05ef4413fc8d91c762b88a
luis <luif.sepulveda@duocuc.cl>
2024-06-26 15:55:09 -0400

    adapatando backencd con front

--- Backend/myproject/myapp/models.py
+++ Backend/myproject/myapp/models.py
@@ -5,13 +5,12 @@ from django.db import models
 class usuario(models.Model):
     id_usuario = models.CharField (max_length=50, primary_key=True, null=False)
     tipo_usuario = models.CharField(max_length=50)
-    nombre = models.CharField(max_length=50)
-    apellido = models.CharField(max_length=50)
-    correo = models.CharField(max_length=50)
-    contrasena = models.CharField(max_length=50)
+    username = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
     
     def __str__(self):
-        return f"{self.id_usuario} {self.tipo_usuario} {self.nombre} {self.apellido}"
+        return f"{self.id_usuario} {self.tipo_usuario} {self.username}{self.password}"
     
 class libro(models.Model):
     isbn = models.CharField(max_length=13, primary_key=True,null=False)
@@ -22,7 +21,7 @@ class libro(models.Model):
     genero = models.CharField(max_length=50)
     precio = models.IntegerField()
     def __str__(self):
-        return f"{self.isbn} {self.titulo} {self.autor} {self.editorial} {self.anio} {self.genero}"
+        return f"{self.isbn} {self.titulo} {self.autor} {self.editorial} {self.anio} {self.genero} {self.precio}"
     
 class boleta(models.Model):
     id_boleta = models.CharField(max_length=50, primary_key=True, null=False)
