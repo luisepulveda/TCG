6b48a59e02093a7af385e9c4be166b85e0bc1887
joseSepulveda <jm.sepulvedas@duocuc.cl>
2024-06-28 16:01:39 -0400

    configuracion simple jwt(incompleto)

--- Backend/myproject/myapp/views.py
+++ Backend/myproject/myapp/views.py
@@ -2,6 +2,7 @@ from rest_framework import viewsets
 from.models import usuario,libro,boleta,detalle_boleta
 from .serializers import usuarioSerializer,libroSerializer,boletaSerializer,detalle_boletaSerializer,Userserializer
 from django.contrib.auth.models import User
+from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework import permissions
 # Create your views here.
 
@@ -10,6 +11,7 @@ class usuarioViewSet(viewsets.ModelViewSet):
     serializer_class=usuarioSerializer
     permission_classes = [permissions.AllowAny]
 
+
 class libroViewSet(viewsets.ModelViewSet):
     queryset = libro.objects.all()
     serializer_class=libroSerializer
@@ -17,10 +19,22 @@ class libroViewSet(viewsets.ModelViewSet):
 class boletaViewSet(viewsets.ModelViewSet):
     queryset = boleta.objects.all()
     serializer_class=boletaSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Task.objects.filter(usuario=self.request.usuario)
+
+    def perform_create(self, serializer):
+        serializer.save(usuario=self.request.usuario)
 
 class detalle_boletaViewSet(viewsets.ModelViewSet):
     queryset = detalle_boleta.objects.all()
     serializer_class=detalle_boletaSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return detalle_boleta.objects.filter(id_boleta=self.kwargs['id_boleta'])
+        
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
--- Backend/myproject/myproject/settings.py
+++ Backend/myproject/myproject/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework_simplejwt',
     'myapp'
 ]
 
@@ -123,3 +124,16 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# configure simple jwt
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    )
+}
+
+
